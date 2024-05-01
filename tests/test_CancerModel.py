c79565b8b8923735cfc8b0addf7244d53b5551b7
Lauren Moon <113121065+Ren487@users.noreply.github.com>
2024-05-01 14:14:44 +0000

    testadd

--- tests/test_CancerModel.py
+++ tests/test_CancerModel.py
@@ -2,15 +2,16 @@ import unittest
 
 from cancer_prediction.cancer_model import CancerModel
 
+
 class TestCancerModel(unittest.TestCase):
 
     def test_diagnosis_to_target(self):
         model = CancerModel()
-        diagnosis = 'Benign'
+        diagnosis = "Benign"
         target = model.diagnosis_to_target(diagnosis)
         self.assertEqual(target, 1)
 
-        diagnosis = 'Malignant'
+        diagnosis = "Malignant"
         target = model.diagnosis_to_target(diagnosis)
         self.assertEqual(target, 0)
 
@@ -18,11 +19,12 @@ class TestCancerModel(unittest.TestCase):
         model = CancerModel()
         target2 = 1
         diagnosis2 = model.target_to_diagnosis(target2)
-        self.assertEqual(diagnosis2, 'Benign')
+        self.assertEqual(diagnosis2, "Benign")
 
         target2 = 0
         diagnosis2 = model.target_to_diagnosis(target2)
-        self.assertEqual(diagnosis2, 'Malignant')
+        self.assertEqual(diagnosis2, "Malignant")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
